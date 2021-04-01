d7608956af63b65a324fe8250f13b60309de285e
ALi-Natiq <46653906+ALi-Natiq@users.noreply.github.com>
2021-04-02 01:26:37 +0200

    Update server.py

--- server.py
+++ server.py
@@ -3,10 +3,10 @@ context = zmq.Context()
 try:
 # Incoming messages come here
 sink = context.socket(zmq.PULL)
-sink.bind("tcp://*:5555") # in plaats van "tcp://*:24041"
+sink.bind("tcp://*:5555") # in plaats van "tcp://*:24041" tijdelijk om locaal te testen
 # Outgoing message go out through here
 publisher = context.socket(zmq.PUB)
-publisher.bind("tcp://*:5556") # in plaats van "tcp://*:24024"
+publisher.bind("tcp://*:5556") # in plaats van "tcp://*:24024" tijdelijk om locaal te testen
 print("Server has started")
 zmq.proxy(sink, publisher)
 except zmq.ZMQError as ex:
