435a342a95e9222d7887d90ff92b8099e016aad3
ALi-Natiq <46653906+ALi-Natiq@users.noreply.github.com>
2021-05-14 15:19:05 +0200

    Update client.cpp
    Een paar commandos toegevoegd.
    Een paar fout meldingen toegevoegd.

--- client.cpp
+++ client.cpp
@@ -1,66 +1,23 @@
+#include <sstream> //istringstream
+#include <stdio.h>
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <cctype>
+#include <cstring>
+#include <utility> // std::pair
+#include <zmq.h>
 #include <zmq.hpp>
 #include <nzmqt/nzmqt.hpp>
-#include <QMutex>
 using namespace std;
-
-int main()
-{
-    cout << "Hello World!" << endl;
-    try
-    {
-        nzmqt::ZMQContext *context = nzmqt::createDefaultContext();
-        nzmqt::ZMQSocket *sub = context->createSocket( nzmqt::ZMQSocket::TYP_SUB, context );
-        nzmqt::ZMQSocket *send = context->createSocket( nzmqt::ZMQSocket::TYP_PUSH, context );
-        sub->connectTo("tcp://192.168.200.212:5554");
-        send->connectTo("tcp://192.168.200.212:5555");
-        sub->subscribeTo("corona>doden>");
-        string naam;
-        string comando;
-        string land;
-        cout << "Geef je naam in " << endl;
-        getline(cin,naam);
-        cout << "Geef je comando in " << endl;
-        getline(cin,comando);
-        cout << "Geef het land in " << endl;
-        getline(cin,land);
-
-        cout << naam << "," << comando <<"," << land <<endl;
-
-
-        while(1){
-
-        }
-
-        if( !send->isConnected() || !sub->isConnected() )
-            {
-                cerr << "NOT CONNECTED !!!" << endl;
-            }
-        nzmqt::ZMQMessage msg = nzmqt::ZMQMessage(QString::fromStdString("?corona>"+naam+">"+comando+">"+land+">").toUtf8());
-        send->sendMessage(msg);
-        //nzmqt::ZMQMessage * incoming = new nzmqt::ZMQMessage();
-
-//        while( sub->isConnected() )
-//        {
-//           sub->receiveMessage(incoming,0);
-//           QString response = QString(incoming->toByteArray());
-//           cout << response.toStdString() <<endl;
-//           // cout << incoming->toByteArray().toStdString() << endl;
-//        }
-    }
-    catch( nzmqt::ZMQException &ex )
-    {
-        std::cerr << "Catched an exception : " << ex.what();
-    }
-    return 0;
-}
+void read_record();
+void * context; //Global context, because you only need one !
 
 vector<vector<string>> parse2DCsvFile(string inputFileName) {
-
     vector<vector<string> > data;
     ifstream inputFile(inputFileName);
     int l = 0;
-
     while (inputFile) {
         l++;
         string line;
@@ -68,7 +25,6 @@ vector<vector<string>> parse2DCsvFile(string inputFileName) {
         if (line[0] != '#') {
             istringstream lineStream(line);
             vector<string> record;
-
             while (lineStream) {
                 string cel;
                 if (!getline(lineStream, cel, ','))
@@ -83,66 +39,119 @@ vector<vector<string>> parse2DCsvFile(string inputFileName) {
                     e.what();
                 }
             }
-
             data.push_back(record);
         }
     }
-
     if (!inputFile.eof()) {
         cerr << "Could not read file " << inputFileName << "\n";
         __throw_invalid_argument("File not found.");
     }
-
     return data;
 }
 
-int main()
-{
-    const int DAY=1 ;
-    const int MONTH=2 ;
-    const int YEAR=3 ;
-    const int CASES=4 ;
-    const int DEATHS=5 ;
-    const int LAND=6 ;
+int main(int argc, const char * argv[]) {
+  std::vector<std::string> listRestaurants;
+    try
+    {
+        nzmqt::ZMQContext *context = nzmqt::createDefaultContext();
+        nzmqt::ZMQSocket *sub = context->createSocket( nzmqt::ZMQSocket::TYP_SUB, context );
+        nzmqt::ZMQSocket *send = context->createSocket( nzmqt::ZMQSocket::TYP_PUSH, context );
+        sub->connectTo("tcp://benternet.backup.pxl-ea-ict.be:24042");//"tcp://192.168.200.212:5554"
+        send->connectTo("tcp://benternet.backup.pxl-ea-ict.be:24041");//"tcp://192.168.200.212:5555"
+        sub->subscribeTo("?corona>");
+        cout << "Covid-19 Service is running ..." << endl;
 
-    string day;
-    string month;
-    string year;
-    string country;
-    string command;
-    cout << "Geef de dag in " << endl;
-    getline(cin,day);
-    cout << "Geef de maand in " << endl;
-    getline(cin,month);
-    cout << "Geef het jaar in " << endl;
-    getline(cin,year);
-    cout << "Geef het land in (Begint met Groot letter) " << endl;
-    getline(cin,country);
-    cout << "Geef het comando in : Voor besmettingen: 1 , Voor doden: 2 " << endl;
-    getline(cin,command);
-
-    vector<vector<string>> data = parse2DCsvFile("C:\\COVID-19-geographic-disbtribution-worldwide.csv");
-
-    for (vector<string> record : data) { //for each loop
-       //for (string x : record)
-        if(command=="1")
+        nzmqt::ZMQMessage * incoming = new nzmqt::ZMQMessage();
+        string naam;
+        string comando;
+        string land;
+        string dag;
+        string maand;
+        string jaar;
+        const int DAY=1 ;
+        const int MONTH=2 ;
+        const int YEAR=3 ;
+        const int CASES=4 ;
+        const int DEATHS=5 ;
+        const int LAND=6 ;
+        const int CONTINENT=10 ;
+        const int CUMULATIVE=11 ;
+        while( sub->isConnected() )
         {
-            if (record[DAY]==day &&record[MONTH]==month && record[YEAR]==year && record[LAND]==country)
-            cout <<"Land: " << record[LAND] <<"\n" << "Datum: "<< record[DAY] <<"/" << record[MONTH] <<"/" << record[YEAR] << "\n" << "Aantal besmetten: "<< record[CASES]<< endl;
-            //else
-            //cout << "Er zijn geen besmettingen voor dit datum" << endl;
+           sub->receiveMessage(incoming,0);
+           QString response = QString(incoming->toByteArray());   
+           QStringList parts =  response.split(">");
+           if (parts[0]=="?corona")
+           {
+               comando=parts[2].toStdString();
+               land=parts[3].toStdString();
+               dag=parts[4].toStdString();
+               maand=parts[5].toStdString();
+               jaar=parts[6].toStdString();
+
+               if(((parts[2]=="1" || parts[2]=="2" || parts[2]=="3" || parts[2]=="4") && islower(parts[3].toStdString()[0])))
+               {
+               naam=parts[1].toStdString();
+               nzmqt::ZMQMessage msg = nzmqt::ZMQMessage(QString::fromStdString("!corona>" + naam + ">" + "Hallo " + naam + ", Sorry! .. Eerste letter van het land dat je ingegeven hebt is klein, het moet GROOT zijn! .. Probeer eens nog een keer! ").toUtf8());
+               send->sendMessage(msg);
+               }
+
+               if((parts[2]=="1" || parts[2]=="2" || parts[2]=="3" || parts[2]=="4" ) && ((parts[4].toStdString())>="15" && parts[5].toStdString()=="12"))
+               {
+               naam=parts[1].toStdString();
+               cout << "Hallo " << naam <<"," << "Je bent op de juiste kanaal!"<< endl;
+               nzmqt::ZMQMessage msg = nzmqt::ZMQMessage(QString::fromStdString("!corona>" + naam + ">" + "Hallo " + naam + ", Sorry! ... De datum die je ingegeven hebt is buiten de grenzen van de database! ").toUtf8());
+               send->sendMessage(msg);
+               }
+
+               vector<vector<string>> data = parse2DCsvFile("C:\\COVID-19-geographic-disbtribution-worldwide.csv");
+               for (vector<string> record : data)
+                {
+
+                    if(parts[2]=="1" && record[LAND]==parts[3].toStdString() && record[DAY]==parts[4].toStdString() && record[MONTH]==parts[5].toStdString() )//&& record[YEAR]==parts[6].toStdString()
+                    {
+                    naam=parts[1].toStdString();
+                    cout << "Hallo " << naam <<"," << "Je bent op de juiste kanaal!"<< endl;
+                    nzmqt::ZMQMessage msg = nzmqt::ZMQMessage(QString::fromStdString("!corona>" + naam + ">" + "Hallo " + naam + "\n"+"Aantal besmettingen " + " van " + land + " op " + dag + "/" + maand + "/" + "2020" + " is " + record[CASES]).toUtf8());
+                    send->sendMessage(msg);                       
+                    }
+
+                    if(parts[2]=="2" && record[LAND]==parts[3].toStdString() && record[DAY]==parts[4].toStdString() && record[MONTH]==parts[5].toStdString() )//&& record[YEAR]==parts[6].toStdString()
+                    {
+                    naam=parts[1].toStdString();
+                    cout << "Hallo " << naam <<"," << "Je bent op de juiste kanaal!"<< endl;
+                    nzmqt::ZMQMessage msg = nzmqt::ZMQMessage(QString::fromStdString("!corona>" + naam + ">" + "Hallo " + naam + ", aantal doden "  + " van " + land + " op " + dag + "/" + maand + "/" + "2020" + " is " + record[DEATHS]).toUtf8());
+                    send->sendMessage(msg);
+                    }
+
+                    if(parts[2]=="3" && record[LAND]==parts[3].toStdString() && record[DAY]==parts[4].toStdString() && record[MONTH]==parts[5].toStdString() )//&& record[YEAR]==parts[6].toStdString()
+                    {
+                    naam=parts[1].toStdString();
+                    cout << "Hallo " << naam <<"," << "Je bent op de juiste kanaal!"<< endl;
+                    nzmqt::ZMQMessage msg = nzmqt::ZMQMessage(QString::fromStdString("!corona>" + naam + ">" + "Hallo " + naam + "\n"+"Aantal besmettingen " + " van " + land +  " (Continent: " + record[CONTINENT]+")"+ " op " + dag + "/" + maand + "/" + "2020" + " is " + record[CASES] +"\n"+"Aantal doden: " + record[DEATHS]).toUtf8());
+                    send->sendMessage(msg);
+                    }
+
+                    if(parts[2]=="4" && record[LAND]==parts[3].toStdString() && record[DAY]==parts[4].toStdString() && record[MONTH]==parts[5].toStdString() )//&& record[YEAR]==parts[6].toStdString()
+                    {
+                    naam=parts[1].toStdString();
+                    cout << "Hallo " << naam <<"," << "Je bent op de juiste kanaal!"<< endl;
+                    nzmqt::ZMQMessage msg = nzmqt::ZMQMessage(QString::fromStdString("!corona>" + naam + ">" + "Hallo " + naam + "\n"+"Aantal besmettingen " + " van " + land +  " (Continent: " + record[CONTINENT]+")"+ " op " + dag + "/" + maand + "/" + "2020" + " is " + record[CASES] +"\n"+"Aantal doden: " + record[DEATHS]+"\n"+"Cumulatief aantal voor 14 dagen Covid-19 gevallen per 100.000: " + record[CUMULATIVE]).toUtf8());
+                    send->sendMessage(msg);
+                    }
+
+               }
+           }
+           //cout << response.toStdString() <<endl;
+           //cout << incoming->toByteArray().toStdString() << endl;
         }
-
-        if(command=="2")
-        {
-            if (record[DAY]==day &&record[MONTH]==month && record[YEAR]==year && record[LAND]==country)
-            cout <<"Land: " << record[LAND] <<"\n" << "Datum: "<< record[DAY] <<"/" << record[MONTH] <<"/" << record[YEAR] << "\n" << "Aantal doden: "<< record[DEATHS]<< endl;
-            //else
-            //cout << "Er zijn geen doden voor dit datum" << endl;
-        }
-
-
     }
+
+    catch( nzmqt::ZMQException &ex )
+    {
+        std::cerr << "Catched an exception : " << ex.what();
+    }
+
 
     return 0;
 }
